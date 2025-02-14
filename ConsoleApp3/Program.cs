155c1266459ce1ee40669e4e3d69a8f0e1ef3f26
MEGA <MEGA@gmail.com>
2025-02-14 18:16:16 +0200

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Въведете число:");
+            int number = int.Parse(Console.ReadLine());
+            if  (IsPowerOfTwo(number)) 
+            { 
+                Console.WriteLine($" {number} е степен на 2.");
+            }
+            else {
+                Console.WriteLine($" {number} не е степен на 2."); 
+                 }
+
+        }
+        static bool IsPowerOfTwo(int n)
+        {
+            if (n <= 0)
+            {
+                return false;
+
+            }
+            if (n == 1)
+            {
+                return true;
+            }  
+            if(n % 2 !=0)
+            {
+                return false;
+                
+            }
+            return IsPowerOfTwo(n / 2);
+        }
+    }
+}
